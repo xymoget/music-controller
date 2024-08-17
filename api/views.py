4f6e5cd48ce88d9dd287267ea0f728b55cb619bc
xymoget <j.rasanov@gmail.com>
2024-08-17 14:18:02 +0300

    react integration

--- api/views.py
+++ api/views.py
@@ -4,6 +4,6 @@ from rest_framework import generics
 from .serializers import RoomSerializer
 from .models import Room
 
-class RoomView(generics.CreateAPIViewgi):
+class RoomView(generics.CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
